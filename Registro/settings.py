3960942d853b254ed870db0aef004807638fa210
porqueseextingieronlosmamuts <yanezmunozmijael@gmail.com>
2025-10-30 16:16:05 -0300

    fix: env import variables

--- Registro/settings.py
+++ Registro/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'Registro.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("dbname"),
-        "USER": os.getenv("user"),
-        "PASSWORD": os.getenv("password"),
-        "HOST": os.getenv("host"),
-        "PORT": os.getenv("port"),
+        "NAME": os.getenv("db_name"),
+        "USER": os.getenv("db_user"),
+        "PASSWORD": os.getenv("db_password"),
+        "HOST": os.getenv("db_host"),
+        "PORT": os.getenv("db_port"),
     }
     
 }
