f3a1b41ce714c43bc6007856b4ae7ba10e6375d0
porqueseextingieronlosmamuts <yanezmunozmijael@gmail.com>
2025-10-30 16:48:27 -0300

    Update admin.py

--- Visitas/admin.py
+++ Visitas/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Visita 
 
-admin.site.register(Visita)
-
 # Register your models here.
+@admin.register(Visita)
+class VisitaAdmin(admin.ModelAdmin):
+ list_display = ('nombre', 'rut', 'motivo_de_visita', 'hora_de_entrada_y_salida')
+ search_fields = ("nombre", "rut") # texto rápido
+ list_filter = ("hora_de_entrada_y_salida",) # filtros laterales
+ ordering = ("nombre",)
+ list_per_page = 25
+
